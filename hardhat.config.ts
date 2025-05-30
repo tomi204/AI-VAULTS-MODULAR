1447407992b2ebc8d801107be7d64e91a8495e49
tomi204 <tomi204@outlook.com>
2025-05-30 21:30:00 +0200

    setup

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+      },
+      {
+        version: "0.8.13",
+      },
+    ],
+  },
+};
+
+export default config;
